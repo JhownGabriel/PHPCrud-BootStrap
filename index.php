6275849975966e68ec26d9c2ef24ad3edd8e5729
JhownGabriel <gabriel4014@outlook.com>
2024-06-10 17:25:24 -0300

    bootstrap

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'header.php';
+?>
+<div class="container">
+    <div class="jumbotron">
+        <h1>Basic CRD in PHP</h1>
+    </div>
+</div>
+
+<?php
+    require_once 'footer.php';
